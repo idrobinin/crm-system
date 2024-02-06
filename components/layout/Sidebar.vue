1b00df8a14c87d1c5d40fc2f87f7a10b42ee8e3d
idrobinin <idrobinin@gmail.com>
2024-02-06 14:14:00 +0800

    set up project

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.svg" alt="" width="140px" class="mx-auto" />
+    </NuxtLink>
+    <a-button ghost>Ghost</a-button>
+  </aside>
+</template>
